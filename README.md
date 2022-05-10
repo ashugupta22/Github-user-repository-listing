058b46e588c888c05119a7d97ee688d991a0ed36
ashutosh <ashutoshgupta2298@gmail.com>
2022-05-10 23:00:02 +0530

    created github repo listing page

---
+++ README.md
@@ -0,0 +1,21 @@
+# Overview
+---
+Created React project to display the Github repositories listing page by passing the Github username.
+# Features
+    1. Disply Github user details.
+    2. Display Github user repositories.
+    3. Display 10 repositories per page.
+    4. Use of pagination
+# Live project deployed link - [demo](demo)
+# How to run project locally
+## Prerequisites 
+- Node.js
+- Npm
+- React.js
+## Steps to run
+1. `npm init` to install all dependencies.
+2. `npm start` to run a local server and preview page.
+<<<<<<< HEAD
+
+=======
+>>>>>>> 7f0e36c0c74d90273a41fdbf972bf3731f634ce7
---
+++ src/components/UserData.js
@@ -0,0 +1,59 @@
+import './style.css';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import UserRepo from './UserRepo';
+import Loading from './Loading';
+function UserData() {
+
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const getUsers = async () => {
+        try{
+            setLoading(false);
+            const response = await fetch('https://api.github.com/users/mojombo');
+        // console.log(response);
+        // const data = await response.json();
+        // console.log(data);
+        setUsers(await response.json());
+        }
+        catch(error){
+            console.log(error);
+        }
+        
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+
+    if (loading) {
+        return <Loading />
+    }
+
+    return (
+        <>
+            <div className='container-fluid'>
+                <div className='container-lg'>
+                    <div className='row justify-content-start mt-5'>
+                        <div className='col-2'>
+                            <img className='roundimg' src={users.avatar_url}></img>
+                        </div>
+                        <div className='col-4 ms-5 my-auto fw-bold'>
+                            <h3 className='t-blue'>{users.name}</h3>
+                            <p>{users.bio}</p>
+                            <p><i className="fa-solid fa-location-dot me-3"></i>{users.location}</p>
+                            <p><span className='me-2'>Twitter:</span><a href={'https://twitter.com/'+users.twitter_username}>{'@'+users.twitter_username}</a></p>
+                        </div>
+                    </div>
+                    <p className='mt-3 fw-bold'><i className="fa-solid fa-link"></i> <a href={users.html_url}>{users.html_url}</a></p>
+
+                    <UserRepo />
+
+                </div>
+            </div>
+        </>
+    )
+}
+export default UserData;
